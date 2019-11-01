37c5419700d5310c9c805802192ecc92a5bd7907
scottb500 <info@retailrider.com>
2019-11-01 11:13:00 -0700

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+//returns response.results with data
+var linkEvents = null;
   fetch('https://hrlink.bubbleapps.io/version-test/api/1.1/obj/link_event')
   .then(res => res.json())
-  .then(json => console.log(json));
+  .then(json => console.log(json))  
+  .catch((err)=>console.log(err));
+
+
+  
+
+
+
+  
+  console.log(linkEvents)
 
